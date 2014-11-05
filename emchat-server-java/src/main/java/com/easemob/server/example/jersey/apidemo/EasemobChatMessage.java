8f8003fdfa7630b75880bc742d5c6d4562853e97
Lynch <sunshinewei_lee@126.com>
2014-11-05 16:09:49 +0800

    rest EasemobChatMessage ok

--- emchat-server-java/src/main/java/com/easemob/server/example/jersey/apidemo/EasemobChatMessage.java
+++ emchat-server-java/src/main/java/com/easemob/server/example/jersey/apidemo/EasemobChatMessage.java
@@ -31,36 +31,76 @@ public class EasemobChatMessage {
 
 	private static final String APPKEY = Constants.APPKEY;
 
+    /**
+     * Main Test
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        /**
+         * 聊天消息 获取最新的20条记录
+         *  curl示例:
+         *  curl -X GET -H "Authorization: Bearer YWMtxc6K0L1aEeKf9LWFzT9xEAAAAT7MNR_9OcNq-GwPsKwj_TruuxZfFSC2eIQ" "https://a1.easemob.com/easemob-demo/chatdemo/chatmessages?ql=order+by+timestamp+desc&limit=20"
+         */
+        ObjectNode queryStrNode = factory.objectNode();
+        queryStrNode.put("ql", "order+by+timestamp+desc");
+        queryStrNode.put("limit", "20");
+        ObjectNode messages = getChatMessages(queryStrNode);
+        System.out.println("聊天消息 获取最新的20条记录: " + messages.toString());
+
+        /**
+         * 聊天消息 获取7天以内的消息
+         * curl示例:
+         * curl -X GET -H "Authorization: Bearer YWMtxc6K0L1aEeKf9LWFzT9xEAAAAT7MNR_9OcNq-GwPsKwj_TruuxZfFSC2eIQ" "https://a1.easemob.com/easemob-demo/chatdemoui/chatmessages?ql=select+*+where+timestamp<1403164734226+and+timestamp></1403164734226+and+timestamp>1403163586000+order+by+timestamp+desc"
+         */
+        String currentTimestamp = String.valueOf(System.currentTimeMillis());
+        String senvenDayAgo = String.valueOf(System.currentTimeMillis() - 7 * 24 * 60 * 60 * 1000);
+        ObjectNode queryStrNode1 = factory.objectNode();
+        queryStrNode1.put("ql", "select * where  timestamp > " + senvenDayAgo + " and timestamp < " + currentTimestamp);
+        ObjectNode messages1 = getChatMessages(queryStrNode1);
+        System.out.println("聊天消息 获取7天以内的消息: " + messages1.toString());
+
+        /**
+         * 聊天消息 分页获取
+         */
+        ObjectNode queryStrNode2 = factory.objectNode();
+        queryStrNode2.put("ql", "order+by+timestamp+desc");
+        queryStrNode2.put("limit", "20");
+        // 第一页
+        ObjectNode messages2 = getChatMessages(queryStrNode2);
+        System.out.println("聊天消息 分页获取 第一页: " + messages2.toString());
+        // 第二页
+        String cursor = messages2.get("cursor").asText();
+        queryStrNode2.put("cursor", cursor);
+        ObjectNode messages3 = getChatMessages(queryStrNode2);
+        System.out.println("聊天消息 分页获取 第二页: " + messages3.toString());
+
+        /**
+         * 获取未读消息数
+         * curl示例
+         * curl -X GET -H "Authorization: Bearer YWMtwIRGSE9gEeSbpNnVBsIhiwAAAUon2XDyEBoBUk6Vg2xm8DZdVjxbhwm7XWY" -i  "https://a1.easemob.com/easemob-demo/chatdemoui/users/v3y0kf9arx/offline_msg_count"
+         */
+        String username = "kenshinn";
+        ObjectNode offlinemsgcountData = getOfflineMsgCountForAppuser(username);
+        System.out.println("获取未读消息数:" + offlinemsgcountData.toString());
+    }
+
 	/**
 	 * 获取聊天消息
 	 * 
-	 * @param appKey
-	 * @param host
-	 * @param token
-	 * @param reqBody
-	 * @param method
-	 * @param uuid
 	 */
 	public static ObjectNode getChatMessages(ObjectNode queryStrNode) {
-
 		ObjectNode objectNode = factory.objectNode();
-
 		// check appKey format
 		if (!JerseyUtils.match("^(?!-)[0-9a-zA-Z\\-]+#[0-9a-zA-Z]+", APPKEY)) {
 			LOGGER.error("Bad format of Appkey: " + APPKEY);
-
 			objectNode.put("message", "Bad format of Appkey");
-
 			return objectNode;
 		}
-
 		try {
-
 			Credentail credentail = new UsernamePasswordCredentail(Constants.APP_ADMIN_USERNAME,
 					Constants.APP_ADMIN_PASSWORD, Roles.USER_ROLE_APPADMIN);
-
-			JerseyWebTarget webTarget = null;
-			webTarget = EndPoints.CHATMESSAGES_TARGET.resolveTemplate("org_name", APPKEY.split("#")[0])
+			JerseyWebTarget webTarget = EndPoints.CHATMESSAGES_TARGET.resolveTemplate("org_name", APPKEY.split("#")[0])
 					.resolveTemplate("app_name", APPKEY.split("#")[1]);
 			if (null != queryStrNode && !StringUtils.isEmpty(queryStrNode.get("ql").asText())) {
 				webTarget.queryParam("ql", queryStrNode.get("ql").asText());
@@ -68,9 +108,7 @@ public class EasemobChatMessage {
 			if (null != queryStrNode && null != queryStrNode.get("limit") &&!StringUtils.isEmpty(queryStrNode.get("limit").asText())) {
 				webTarget.queryParam("limit", queryStrNode.get("limit").asText());
 			}
-
 			objectNode = JerseyUtils.sendRequest(webTarget, null, credentail, HTTPMethod.METHOD_GET, null);
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -78,36 +116,29 @@ public class EasemobChatMessage {
 		return objectNode;
 	}
 
-	/**
-	 * Main Test
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
+    /**
+     * 获取一个IM用户的未读消息数
+     *
+     */
+    public static ObjectNode getOfflineMsgCountForAppuser(String username) {
+        ObjectNode objectNode = factory.objectNode();
+        // check appKey format
+        if (!JerseyUtils.match("^(?!-)[0-9a-zA-Z\\-]+#[0-9a-zA-Z]+", APPKEY)) {
+            LOGGER.error("Bad format of Appkey: " + APPKEY);
+            objectNode.put("message", "Bad format of Appkey");
+            return objectNode;
+        }
+        try {
+            Credentail credentail = new UsernamePasswordCredentail(Constants.APP_ADMIN_USERNAME,
+                    Constants.APP_ADMIN_PASSWORD, Roles.USER_ROLE_APPADMIN);
+            JerseyWebTarget webTarget = EndPoints.USERS_TARGET.resolveTemplate("org_name", APPKEY.split("#")[0])
+                    .resolveTemplate("app_name", APPKEY.split("#")[1]).path(username).path("offline_msg_count");
+            objectNode = JerseyUtils.sendRequest(webTarget, null, credentail, HTTPMethod.METHOD_GET, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		// 聊天消息 获取最新的20条记录
-		ObjectNode queryStrNode = factory.objectNode();
-		queryStrNode.put("ql", "order+by+timestamp+desc");
-		queryStrNode.put("limit", "20");
-		ObjectNode messages = getChatMessages(queryStrNode);
+        return objectNode;
+    }
 
-		// 聊天消息 获取7天以内的消息
-		String currentTimestamp = String.valueOf(System.currentTimeMillis());
-		String senvenDayAgo = String.valueOf(System.currentTimeMillis() - 7 * 24 * 60 * 60 * 1000);
-		ObjectNode queryStrNode1 = factory.objectNode();
-		queryStrNode1.put("ql", "select * where  timestamp > " + senvenDayAgo + " and timestamp < " + currentTimestamp);
-		ObjectNode messages1 = getChatMessages(queryStrNode1);
-
-		// 聊天消息 分页获取
-		ObjectNode queryStrNode2 = factory.objectNode();
-		queryStrNode2.put("ql", "order+by+timestamp+desc");
-		queryStrNode2.put("limit", "20");
-		// 第一页
-		ObjectNode messages2 = getChatMessages(queryStrNode2);
-		// 第二页
-		String cursor = messages2.get("cursor").asText();
-		queryStrNode2.put("cursor", cursor);
-		ObjectNode messages3 = getChatMessages(queryStrNode2);
-
-	}
 }
